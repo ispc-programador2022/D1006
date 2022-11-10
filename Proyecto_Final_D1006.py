69b78fddc4e2be81b2887f42aafa745db70c342d
Angel Villarreal <crangelvillarrealbarroso@gmail.com>
2022-11-10 01:06:54 -0300

    Agregado trabajo sobre el codigo

--- Proyecto_Final_D1006.py
+++ Proyecto_Final_D1006.py
@@ -11,10 +11,89 @@ pip install lxml
 
 from bs4 import BeautifulSoup
 
+import glob
+
 #MODIFICAR EL NOMBRE DEL ARCHIVO CON LA RUTA:
-with open('C:/Users/angel/PROYECTO FINAL/HTML/pagina1.html', 'r', encoding="utf-8") as html_file:
-     content = html_file.read()
+datos = []
+product1 = []
+brand1 = []
+seller1 = []
+#financial_mode1 = [] todas dicen 18 cuotas así que no es relevante
+financing_amount1 = []
+#price1 = [] al final el precio termina siendo el de sale price
+sale_price1 = []
 
-soup = BeautifulSoup(content, 'lxml')
+for file in glob.glob("C:/Users/angel/PROYECTO INTEGRADOR D1006/D1006/HTML/*.html"):
+    with open(file, 'r', encoding="utf8", errors='ignore') as html_file:
+        content = html_file.read()
+        soup = BeautifulSoup(content, 'lxml')
 
-print(soup)
+    bloques = soup.find_all('article', attrs={"id": "modern-variant-card"})
+
+    for bloque in bloques:
+        product = ""
+        seller = ""
+        brand = ""
+        financial_mode = ""
+        financing_amount = 0
+        price = 0
+        sale_price = 0
+
+        valor = bloque.find('h6',  class_="d-inline-block")
+        if valor:
+            product = valor.text
+
+        valor = bloque.find('div',  class_="shop-panel d-block mt-auto")
+        if valor:
+            seller = valor.text.replace("-", "").strip()
+
+        valor = bloque.find('small',  class_="ng-star-inserted")
+        if valor:
+            f = valor.text.split("$")
+            financial_mode = f[0]
+            financing_amount = float(f[1].replace(
+                "$", "").replace(".", "").replace(",", "."))
+
+        valor = bloque.find(
+            'div',  class_="manufacturer-panel d-block mt-auto")
+        if valor:
+            brand = valor.text.strip()
+
+        # Trata de recuperar los precios unificados, si no encuenta valor, es porque vienen por separado, precio de oferta y precio regular
+        valor = bloque.find(
+            'span',  class_="price sale-price ng-star-inserted")
+        if valor:
+            price = float(valor.text.replace(
+                "$", "").replace(".", "").replace(",", "."))
+            sale_price = float(valor.text.replace(
+                "$", "").replace(".", "").replace(",", "."))
+        else:
+            valor = bloque.find('span',  class_="price ng-star-inserted")
+            if valor:
+                price = float(valor.text.replace(
+                    "$", "").replace(".", "").replace(",", "."))
+
+            valor = bloque.find('span',  class_="sale-price ng-star-inserted")
+            if valor:
+                sale_price = float(valor.text.replace(
+                    "$", "").replace(".", "").replace(",", "."))
+
+        datos.append({"product": product, "brand": brand, "seller": seller, "financial_mode": financial_mode,"financing_amount": financing_amount, "price": price, "sale_price": sale_price})
+        #datos.append({product, brand, seller, financial_mode, financing_amount, price, sale_price})
+        product1.append(product)
+        brand1.append(brand)
+        seller1.append(seller)
+        #financial_mode1.append(financial_mode)
+        financing_amount1.append(financing_amount)
+        #price1.append(price)
+        sale_price1.append(sale_price)
+
+#print(datos)
+
+import pandas as pd
+
+dict = {'product': product1, 'brand': brand1, 'seller': seller1, 'financing_amount': financing_amount1,'sale_price': sale_price1}
+df = pd.DataFrame((dict), index=list(range(1,147)))
+df.to_csv('database.csv', encoding="utf8")
+
+print(df)
