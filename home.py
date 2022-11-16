ecd64724c919a511aeb986cbbb87803299ed46c1
Angel Villarreal <crangelvillarrealbarroso@gmail.com>
2022-11-15 23:08:15 -0300

    Subiendo archivos.

--- home.py
+++ home.py
@@ -1,4 +1,6 @@
 
+
+
 import csv
 with open('C:/Users/angel/PROYECTO INTEGRADOR D1006/D1006/database.csv', encoding="utf8", newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|', )
